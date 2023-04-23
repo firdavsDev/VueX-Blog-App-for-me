d6317fe8237339f7f5412f363801626d147c6bc6
Davronbek Boltayev <xackercoder@gmail.com>
2023-04-23 18:08:08 +0500

    VueX blog app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>
+      <Navbar />
+    </div>
+  </div>
+</template>
+<script>
+import { Navbar } from '@/components'
+
+export default {
+  name: 'App',
+
+  components: {
+    Navbar
+  },
+
+}
+</script>
---
+++ test.py
@@ -0,0 +1,7 @@
+
+def print_tub(n):
+    for num in range(2,n):
+        if all(num % i != 0 for i in range(2, int((num**1/2))+1)):
+            print(num)
+
+print_tub(10)
